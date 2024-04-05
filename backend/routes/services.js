d0b7fd861bbad550f390e0249e41d32b713f92a7
Entony Maxwell <entonymaxwell01@gmail.com>
2024-04-05 19:10:57 -0300

    Adicionando rota post de criação de eventos

--- backend/routes/services.js
+++ backend/routes/services.js
@@ -2,4 +2,8 @@ const router = require("express").Router();
 
 const serviceController = require("../controllers/serviceController.js");
 
+router.route("/services").post((req,res) =>{
+    serviceController.create(req,res);
+});
+
 module.exports = router;
